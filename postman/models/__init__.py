05e840bc6e2ad9561a5c3b2e5341ec62aebf7225
Slim <mslimbeji@gmail.com>
2022-02-09 10:53:27 +0100

    adding helper to initiate tortoise connection outside fastapi

--- postman/models/__init__.py
+++ postman/models/__init__.py
@@ -1,1 +1,14 @@
+from tortoise import Tortoise
+
+from postman.config import get_settings
+
 from .models import ApiItem, Endpoint, QueryParam, Role, Tag, User
+
+
+async def init_db():
+    settings = get_settings()
+    await Tortoise.init(
+        db_url=settings.DATABASE_URL,
+        modules=settings.TORTOISE_MODELS,
+    )
+    await Tortoise.generate_schemas()
