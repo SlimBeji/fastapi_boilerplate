6956a6a80bfde920a3ff63c5708fc8a7627b7b04
Slim <mslimbeji@gmail.com>
2022-02-13 20:15:25 +0100

    remove default index page in main.py

--- main.py
+++ main.py
@@ -4,11 +4,5 @@ from postman.app import create_app
 
 app = create_app()
 
-
-@app.get("/")
-async def index():
-    return {"message": "Hello World!", "name": "Slim Beji"}
-
-
 if __name__ == "__main__":
     uvicorn.run(app)
