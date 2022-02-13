9938ac6139a575ca977639116c37e977daec392a
Slim <mslimbeji@gmail.com>
2022-02-13 19:06:18 +0100

    adding .env support to postman.config

--- postman/config.py
+++ postman/config.py
@@ -1,12 +1,11 @@
-import os
 from functools import lru_cache
 
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
-    REDIS_URL: str = os.getenv("REDIS_URL")
+    DATABASE_URL: str
+    REDIS_URL: str
 
     TORTOISE_MODELS: dict = {
         "models": ["postman.models.models"],
@@ -14,6 +13,10 @@ class Settings(BaseSettings):
     TORTOISE_CONNECTION_NAME: str = "default"
 
     MAX_ITEM_PER_RESPONSE = 20
+
+    class Config:
+        env_file = "../.env"
+
 
 
 @lru_cache()
