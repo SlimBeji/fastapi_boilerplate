d1b11b7a8224ae6d68d78bcdfa5f78b0b45f0041
Slim <mslimbeji@gmail.com>
2023-09-22 12:16:23 +0100

    update indew router

--- backend/api/index.py
+++ backend/api/index.py
@@ -1,15 +1,8 @@
-from typing import Union
-
 from fastapi import APIRouter
 
-index_router = APIRouter(prefix="/api")
+index_router = APIRouter(prefix="/api", tags=["Hello World"])
 
 
 @index_router.get("/")
-def read_root():
+def hello_world():
     return {"Hello": "World"}
-
-
-@index_router.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
