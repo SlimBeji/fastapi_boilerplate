0aa5b710bfcfd6dca9e7d1fda2556fd6a89a6ca2
Slim <mslimbeji@gmail.com>
2023-09-22 21:21:52 +0100

    linting

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,6 +1,6 @@
 from tortoise import Tortoise
-from backend.config import settings
 
+from backend.config import settings
 from backend.models.api_item.model import ApiItem
 from backend.models.endpoint.model import Endpoint
 from backend.models.query_param.model import QueryParam
