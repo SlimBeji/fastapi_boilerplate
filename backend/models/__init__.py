6309831606b4933dc15a050d382fda578316bc1c
Slim <mslimbeji@gmail.com>
2023-09-22 15:43:31 +0100

    fix tortoise models discovery bugs

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,7 +1,11 @@
-# Needed for Tortoise model initialization
+from tortoise import Tortoise
+from backend.config import settings
+
 from backend.models.api_item.model import ApiItem
 from backend.models.endpoint.model import Endpoint
 from backend.models.query_param.model import QueryParam
 from backend.models.role.model import Role
 from backend.models.tag.model import Tag
 from backend.models.user.model import User
+
+Tortoise.init_models(settings.TORTOISE_MODELS["models"], "models")
